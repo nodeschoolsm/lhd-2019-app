ee034892ef4319e9035d7a775a6a98c75f7cde8e
Christopher Fuentes <fuenteschris75@gmail.com>
2019-11-26 18:43:21 -0600

    Initial commit

---
+++ Local_Hack_Day/cordova/platforms/android/app/src/main/assets/www/precache-manifest.5658bc2ad9b160e2b688ba6ee1828b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1020fd5281836ee783890263aa04275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88c7adf75cce6ff04f9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7d3a1447a68f0d01f277",
+    "url": "/static/js/2.b28ba69b.chunk.js"
+  },
+  {
+    "revision": "f88c7adf75cce6ff04f9",
+    "url": "/static/js/main.f4333e71.chunk.js"
+  },
+  {
+    "revision": "296fbfebbcd235be1e2e",
+    "url": "/static/js/runtime-main.26c03792.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ Local_Hack_Day/react-js/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Layout } from 'antd';
+import {Head} from './components/Header'
+
+import {RegisterTeam} from './components/RegisterTeam'
+
+const { Content } = Layout
+
+function App() {
+  return (
+    <div className="App">
+    <Layout style={{backgroundColor:'white'}}>
+      <Head/>
+      <Content style={{ padding: '0 8px', marginTop: 150, height: '100vh' }}>
+        <RegisterTeam></RegisterTeam>
+     </Content>
+      </Layout>
+    </div>
+
+  );
+}
+
+export default App;
